2276f5e5034868d2fa9bb42984332df02c47084f
std3 <67806187+standard3@users.noreply.github.com>
2024-04-22 00:04:24 +0200

    feat: implemented constructors and generic derives

--- src/introspection/segment.rs
+++ src/introspection/segment.rs
@@ -1,16 +1,28 @@
 /// This module contains the structs and functions to introspect a segment (memory mapping).
 use std::path::Path;
 
+// TODO:
+// - implement common traits on structs (e.g. Debug, Clone, PartialEq, Eq, ToString + FromStr, Display)
+
 pub type InodeId = u64;
 
 /// Small device abstrcation.
 /// See https://linux-kernel-labs.github.io/refs/heads/master/labs/device_model.html#classes
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Device {
     major: u32,
     minor: u32,
 }
 
+impl Device {
+    /// Create a new device.
+    pub fn new(major: u32, minor: u32) -> Self {
+        Device { major, minor }
+    }
+}
+
 /// Information about a segment in the process's virtual address space.
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum SegmentType {
     /// The initial process's (also known as the main thread's) stack.
     Stack,
@@ -25,6 +37,7 @@ pub enum SegmentType {
 }
 
 /// Type of data segment.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum DataSegment {
     /// The process's heap.
     Heap,
@@ -33,6 +46,7 @@ pub enum DataSegment {
 }
 
 /// Permissions for a segment.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum SegmentPermission {
     Read,
     Write,
@@ -43,6 +57,7 @@ pub enum SegmentPermission {
 }
 
 /// Mapped memory region in the process's virtual address space.
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Segment {
     /// Start address
     start: u64,
@@ -56,6 +71,73 @@ pub struct Segment {
     device: Option<Device>,
     /// Inode on that device
     inode: Option<InodeId>,
+    /// Type of the segment
+    segment_type: SegmentType,
     /// Usually the file that is backing the mapping
-    path: Path,
+    path: Box<Path>,
+}
+
+impl Segment {
+    /// Create a new segment.
+    pub fn new(
+        start: u64,
+        end: u64,
+        permissions: [SegmentPermission; 4],
+        offset: u64,
+        device: Option<Device>,
+        inode: Option<InodeId>,
+        segment_type: SegmentType,
+        path: Box<Path>,
+    ) -> Self {
+        Segment {
+            start,
+            end,
+            permissions,
+            offset,
+            device,
+            inode,
+            segment_type,
+            path,
+        }
+    }
+
+    /// Get the start address of the segment.
+    pub fn start(&self) -> u64 {
+        self.start
+    }
+
+    /// Get the end address of the segment.
+    pub fn end(&self) -> u64 {
+        self.end
+    }
+
+    /// Get the permissions of the segment.
+    pub fn permissions(&self) -> &[SegmentPermission; 4] {
+        &self.permissions
+    }
+
+    /// Get the offset of the segment.
+    pub fn offset(&self) -> u64 {
+        self.offset
+    }
+
+    /// Get the device of the segment.
+    pub fn device(&self) -> Option<Device> {
+        self.device.clone()
+    }
+
+    /// Get the inode of the segment.
+    pub fn inode(&self) -> Option<InodeId> {
+        self.inode
+    }
+
+    /// Get the type of the segment.
+    pub fn segment_type(&self) -> &SegmentType {
+        &self.segment_type
+    }
+
+    /// Get the path of the segment.
+    pub fn path(&self) -> &Path {
+        &self.path
+    }
 }
