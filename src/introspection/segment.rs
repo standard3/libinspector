0ae063ca57c1910b6e12acdb17589704e2909c53
std3 <67806187+standard3@users.noreply.github.com>
2024-04-22 18:33:32 +0200

    feat: multiple traits implementation and get_from_pid()

--- src/introspection/segment.rs
+++ src/introspection/segment.rs
@@ -1,11 +1,42 @@
 /// This module contains the structs and functions to introspect a segment (memory mapping).
+use anyhow::Result;
 use std::{
+    error::Error,
     fmt::Display,
+    fs::File,
+    io::{BufRead, BufReader},
+    num::ParseIntError,
     path::{Path, PathBuf},
     str::FromStr,
 };
 
+use super::process::Pid;
+
 pub type InodeId = u64;
+
+#[derive(Debug)]
+pub enum SegmentParseError {
+    ParseIntError(ParseIntError),
+    ParseError(String),
+}
+
+impl Display for SegmentParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            SegmentParseError::ParseIntError(e) => write!(f, "ParseIntError: {}", e),
+            SegmentParseError::ParseError(s) => write!(f, "ParseError: {}", s),
+        }
+    }
+}
+
+impl Error for SegmentParseError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            SegmentParseError::ParseIntError(e) => Some(e),
+            SegmentParseError::ParseError(_) => None,
+        }
+    }
+}
 
 /// Small device abstraction.
 /// See <https://linux-kernel-labs.github.io/refs/heads/master/labs/device_model.html#classes>
@@ -82,7 +113,7 @@ impl Display for SegmentType {
 }
 
 impl FromStr for SegmentType {
-    type Err = ();
+    type Err = SegmentParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         if s.starts_with("[anon:") && s.ends_with(']') {
@@ -96,7 +127,10 @@ impl FromStr for SegmentType {
                 "[stack]" => Ok(SegmentType::Stack),
                 "[vdso]" => Ok(SegmentType::SharedLibrary),
                 "[heap]" => Ok(SegmentType::Data(DataSegment::Heap)),
-                _ => Err(()),
+                _ => Err(SegmentParseError::ParseError(format!(
+                    "Unknown segment type: {}",
+                    s
+                ))),
             }
         } else {
             Ok(SegmentType::Code(Box::new(Path::new(&s).to_path_buf())))
@@ -137,6 +171,25 @@ impl Display for SegmentPermission {
     }
 }
 
+impl FromStr for SegmentPermission {
+    type Err = SegmentParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "r" => Ok(SegmentPermission::Read),
+            "w" => Ok(SegmentPermission::Write),
+            "x" => Ok(SegmentPermission::Execute),
+            "-" => Ok(SegmentPermission::NoPermission),
+            "p" => Ok(SegmentPermission::Private),
+            "s" => Ok(SegmentPermission::Shared),
+            _ => Err(SegmentParseError::ParseError(format!(
+                "Unknown segment permission: {}",
+                s
+            ))),
+        }
+    }
+}
+
 /// Mapped memory region in the process's virtual address space.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Segment {
@@ -149,9 +202,9 @@ pub struct Segment {
     /// Offset into the file/whatever
     pub offset: u64,
     /// Device (major:minor)
-    pub device: Option<Device>,
+    pub device: Device,
     /// Inode on that device
-    pub inode: Option<InodeId>,
+    pub inode: InodeId,
     /// Usually the file that is backing the mapping
     pub pathname: SegmentType,
 }
@@ -163,8 +216,8 @@ impl Segment {
         end: u64,
         permissions: [SegmentPermission; 4],
         offset: u64,
-        device: Option<Device>,
-        inode: Option<InodeId>,
+        device: Device,
+        inode: InodeId,
         pathname: SegmentType,
     ) -> Self {
         Segment {
@@ -193,4 +246,75 @@ impl Display for Segment {
             self.pathname,
         )
     }
+}
+
+impl FromStr for Segment {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let mut parts = s.split_whitespace();
+
+        let address = parts
+            .next()
+            .ok_or(SegmentParseError::ParseError("No address".to_string()))?;
+        let permissions = parts
+            .next()
+            .ok_or(SegmentParseError::ParseError("No permissions".to_string()))?;
+        let offset = parts
+            .next()
+            .ok_or(SegmentParseError::ParseError("No offset".to_string()))?;
+        let device = parts
+            .next()
+            .ok_or(SegmentParseError::ParseError("No device".to_string()))?;
+        let inode = parts
+            .next()
+            .ok_or(SegmentParseError::ParseError("No inode".to_string()))?;
+        let pathname = parts
+            .next()
+            .ok_or(SegmentParseError::ParseError("No pathname".to_string()))?;
+
+        // Addresses range
+        let addresses: Vec<&str> = address.split('-').collect();
+        let start = u64::from_str_radix(addresses[0], 16)?;
+        let end = u64::from_str_radix(addresses[1], 16)?;
+
+        // Permissions
+        let permissions = [
+            SegmentPermission::from_str(&permissions[0..1])?,
+            SegmentPermission::from_str(&permissions[1..2])?,
+            SegmentPermission::from_str(&permissions[2..3])?,
+            SegmentPermission::from_str(&permissions[3..4])?,
+        ];
+
+        let offset = u64::from_str_radix(offset, 16)?;
+        let device = Device::from_str(device)?;
+        let inode = u64::from_str(inode)?;
+        let pathname = SegmentType::from_str(pathname)?;
+
+        Ok(Segment::new(
+            start,
+            end,
+            permissions,
+            offset,
+            device,
+            inode,
+            pathname,
+        ))
+    }
+}
+
+pub fn get_from_pid(pid: Pid) -> Result<Vec<Segment>> {
+    let maps_path = format!("/proc/{}/maps", pid);
+    let maps_file = File::open(maps_path)?;
+    let maps_reader = BufReader::new(maps_file);
+    let mut segments = Vec::new();
+
+    for line in maps_reader.lines() {
+        let line = line?;
+        let segment = Segment::from_str(&line)?;
+
+        segments.push(segment);
+    }
+
+    Ok(segments)
 }
